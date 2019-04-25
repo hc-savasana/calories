a2a1eb73eec080859b3701e30e88399a0a78635e
yidahc <46684324+yidahc@users.noreply.github.com>
2019-04-25 10:43:38 -0500

    Revert "Yidah2branch"

--- client/src/components/FoodSearch.jsx
+++ client/src/components/FoodSearch.jsx
@@ -1,59 +1,68 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-
-const apiURL = 'https://api.edamam.com/api/food-database/parser?ingr=';
-const apiKey = '&app_id=6faf9cc7&app_key=5c9b29569582527e3c87130f130cb58d';
-
-
-class FoodSearch extends Component {
-  constructor(props) {
-  	super(props);
-    this.state = {
-    query: '',
-    results: []
-    }
-  this.handleInput = this.handleInput.bind(this);
-  this.findAndReplace = this.findAndReplace.bind(this);
-  this.getInfo = this.getInfo.bind(this);
-  }
-    
-  findAndReplace = (string, target, replacement) => {
+import React, { Component } from 'react';
+  import Header from './Header.jsx';
+  
+  
+  const API_URL = 'https://api.edamam.com/api/food-database/parser?ingr=';
+   const apiKey = "&app_id=6faf9cc7&app_key=5c9b29569582527e3c87130f130cb58d";
+  
+  class FoodSearch extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        entry: '',
+        hints: '',
+      }
+      }
+      
+      /*
+      componentWillReceiveProps(nextProps) {
+      this.setState({
+        hints: this.props.items,
+      });
+      };
+  */
+  
+   findAndReplace(string, target, replacement) {
     for (var i = 0; i < string.length; i++) {
-      var s = string.replace(target, replacement);
-    };
-    return s;
-  }
-
-  handleInput= () => {
-    this.setState({
-      query: this.search.value
-    });
-    if (this.state.query.length >= 2) {
-    this.getInfo(this.state.query);
+      s = string.replace(target, replacement);
     }
+    return string;
   }
-
-  getInfo = (ingredient) => {
-  var ing = this.findAndReplace(ingredient, " ", "%20");
-    axios.get(`${apiURL}${ing}${apiKey}`)
-    .then(({ data }) => {
-        data.hints.map(e => this.state.results.push(e.food.label))
-      }) 
-  }
-
-
- render() {
-   return (
-     <form>
-       <input
-         placeholder="Search for..."
-         ref={input => this.search = input}
-         onChange={this.handleInput}
-       />
-       <p>{this.state.results}</p>
-     </form>
-   )
- }
-}
-
-export default FoodSearch
+  
+  
+   fetchHints = ingredient => {
+    var ing = findAndReplace(ingredient, " ", "%20");
+      const url =  `${apiURL}${ing}${apiKey}`;
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          hints: data
+          });
+        })
+  };
+  
+    render() 
+    {
+      return (
+          <div>
+          <Header />
+          <input 
+            type="text" 
+            className="input" 
+            placeholder="Search..." 
+          />
+          <div className="content">
+            <div className="container">
+              <section className="section">
+                <ul>
+                  <li>stuff</li>
+                </ul>
+              </section>
+            </div>
+          </div>
+        </div>
+      )
+      }
+    }
+  export default FoodSearch;
